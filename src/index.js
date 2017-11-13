9091c57a8d36228846ba668b5c9b783aec149eaa
nicholekirlin <nichole.kirlin@gmail.com>
2017-11-13 17:05:05 -0600

    added loading message

--- src/index.js
+++ src/index.js
@@ -10,12 +10,14 @@ sayHello('World');
 const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
+  $("h1").text("Welcome to MovieLister");
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
+  console.log(movies);
+
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
-
