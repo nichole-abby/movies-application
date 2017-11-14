27bf9ca6d4db7ea6013d3ea10f1509dfbd888498
nicholekirlin <nichole.kirlin@gmail.com>
2017-11-14 11:11:24 -0600

    movies on browser page!

--- src/index.js
+++ src/index.js
@@ -10,12 +10,19 @@ sayHello('World');
 const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
-  $("h1").text("Welcome to MovieLister");
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-  console.log(movies);
+    $("h1").text("Welcome to MovieLister");
+    let htmlString = "";
+    // console.log(movies);
+    movies.forEach(function(movie) {
+        htmlString += "<h3>" + movie.title + "</h3>"
+            + "<p>Rating: " + movie.rating + "</p>";
+        // console.log(`id#${id} - ${title} - rating: ${rating}`);
+        // $("p").text("Here are the movies");
+    });
+
+    $(".moviesHere").html(htmlString);
+
+  // console.log(movies);
 
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
