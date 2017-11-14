6868859b68e20d89e2351a4c684b5b8c583d2831
abbyperez <abbyperez28@gmail.com>
2017-11-14 16:05:00 -0600

    added the form input and update list of movies with new input.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 /**
  * es6 modules and imports
  */
-import sayHello from './hello';
-sayHello('World');
+// import sayHello from './hello';
+// sayHello('World');
 
 /**
  * require style imports
@@ -27,4 +27,57 @@ getMovies().then((movies) => {
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
+});
+
+$("#addMovie").click(function(event) {
+    event.preventDefault();
+    addMovie();
+
+
+function addMovie() {
+    let title = $("#movie").val();
+    let rating = $("#rating").val();
+
+    const movieRating = {title: title, rating: rating};
+    const url = '/api/movies';
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(movieRating),
+    };
+    fetch(url, options)
+        .then(function(){
+
+            let title = $("#movie").val("");
+            let rating = $("#rating").val("");
+            getMovies().then((movies) => {
+                $("h1").text("Welcome to MovieLister");
+                let htmlString = "";
+                // console.log(movies);
+                movies.forEach(function(movie) {
+                    htmlString += "<h3>" + movie.title + "</h3>"
+                        + "<p>Rating: " + movie.rating + "</p>";
+                    // console.log(`id#${id} - ${title} - rating: ${rating}`);
+                    // $("p").text("Here are the movies");
+                });
+
+                $(".moviesHere").html(htmlString);
+
+                // console.log(movies);
+
+            }).catch((error) => {
+                alert('Oh no! Something went wrong.\nCheck the console for details.')
+                console.log(error);
+            });
+
+        })
+        .catch(/* handle errors */);
+
+
+}
+
+
+
 });
